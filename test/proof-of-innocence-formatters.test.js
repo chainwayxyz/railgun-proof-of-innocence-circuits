068839ea8cc24a608c2bf3377baf5445bd24cbde
jrmeurer <jrmeurer@wisc.edu>
2023-09-26 09:58:11 -0600

    Add updated format tests

---
+++ test/proof-of-innocence-formatters.test.js
@@ -0,0 +1,121 @@
+const testVector = require('./test-format-vector-poi.json');
+
+const tester = require('circom_tester').wasm;
+
+const isPrefixed = (str) => str.startsWith('0x');
+const prefix0x = (str) => (isPrefixed(str) ? str : `0x${str}`);
+function hexToBigInt(str) {
+  return BigInt(prefix0x(str));
+}
+
+const padWithZerosToMax = (
+  array,
+  max,
+  zeroValue = 2051258411002736885948763699317990061539314419500486054347250703186609807356n,
+) => {
+  const padded = [...array];
+  while (padded.length < max) {
+    padded.push(zeroValue);
+  }
+  return padded;
+};
+
+const padWithArraysOfZerosToMaxAndLength = (
+  array,
+  max,
+  length,
+  zeroValue = 2051258411002736885948763699317990061539314419500486054347250703186609807356n,
+) => {
+  const padded = [...array];
+  while (padded.length < max) {
+    padded.push(new Array(length).fill(zeroValue));
+  }
+  return padded;
+};
+
+const formatInputs = (proofInputs) => {
+  const maxInputs = 13;
+  const maxOutputs = 13;
+
+  return {
+    anyRailgunTxidMerklerootAfterTransaction: hexToBigInt(
+      proofInputs.anyRailgunTxidMerklerootAfterTransaction,
+    ),
+    boundParamsHash: hexToBigInt(proofInputs.boundParamsHash),
+    nullifiers: padWithZerosToMax(
+      proofInputs.nullifiers.map(hexToBigInt),
+      maxInputs,
+    ),
+    commitmentsOut: padWithZerosToMax(
+      proofInputs.commitmentsOut.map(hexToBigInt),
+      maxOutputs,
+    ),
+    spendingPublicKey: proofInputs.spendingPublicKey.map(BigInt),
+    nullifyingKey: BigInt(proofInputs.nullifyingKey),
+    token: hexToBigInt(proofInputs.token),
+    randomsIn: padWithZerosToMax(
+      proofInputs.randomsIn.map(hexToBigInt),
+      maxInputs,
+    ),
+    valuesIn: padWithZerosToMax(
+      proofInputs.valuesIn.map(BigInt),
+      maxInputs,
+      0n, // Use Zero = 0 here
+    ),
+    utxoPositionsIn: padWithZerosToMax(
+      proofInputs.utxoPositionsIn.map(BigInt),
+      maxInputs,
+    ),
+    blindedCommitmentsIn: padWithZerosToMax(
+      proofInputs.blindedCommitmentsIn.map(hexToBigInt),
+      maxInputs,
+      0n, // Use Zero = 0 here
+    ),
+    creationTxidsIn: padWithZerosToMax(
+      proofInputs.creationTxidsIn.map(hexToBigInt),
+      maxInputs,
+    ),
+    npksOut: padWithZerosToMax(proofInputs.npksOut.map(BigInt), maxOutputs),
+    valuesOut: padWithZerosToMax(
+      proofInputs.valuesOut.map(BigInt),
+      maxOutputs,
+      0n, // Use Zero = 0 here
+    ),
+    railgunTxidMerkleProofIndices: hexToBigInt(
+      proofInputs.railgunTxidMerkleProofIndices,
+    ),
+    railgunTxidMerkleProofPathElements:
+      proofInputs.railgunTxidMerkleProofPathElements.map(hexToBigInt),
+    poiMerkleroots: padWithZerosToMax(
+      proofInputs.poiMerkleroots.map(hexToBigInt),
+      maxInputs,
+    ),
+    poiInMerkleProofIndices: padWithZerosToMax(
+      proofInputs.poiInMerkleProofIndices.map(hexToBigInt),
+      maxInputs,
+      0n, // Use Zero = 0 here
+    ),
+    poiInMerkleProofPathElements: padWithArraysOfZerosToMaxAndLength(
+      proofInputs.poiInMerkleProofPathElements.map((pathElements) =>
+        pathElements.map(hexToBigInt),
+      ),
+      maxInputs,
+      16,
+    ),
+  };
+};
+
+describe('Proof of Innocence formatters', async () => {
+  it('Should test POI circuit with mock formatted inputs', async () => {
+    const circuit = await tester('./circuits/poi-transaction.circom', {
+      reduceConstraints: false,
+    });
+
+    const inputs = testVector;
+
+    const inputsBigInt = formatInputs(inputs);
+
+    const witness = await circuit.calculateWitness(inputsBigInt);
+    await circuit.checkConstraints(witness);
+  });
+});
